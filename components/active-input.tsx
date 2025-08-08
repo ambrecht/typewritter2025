9ad2c985c4798848b24cb24278bd2e0d1662415d
Tino Ambrecht <58052162+ambrecht@users.noreply.github.com>
2025-08-08 16:45:52 +0200

    Bind active line height to lineHpx

--- components/active-input.tsx
+++ components/active-input.tsx
@@ -3,11 +3,17 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const ActiveInput = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn(className)} {...props} />
-  )
-)
+const ActiveInput = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, style, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(className)}
+    style={{ height: "var(--lineHpx)", lineHeight: "var(--lineHpx)", ...style }}
+    {...props}
+  />
+))
 ActiveInput.displayName = "ActiveInput"
 
 export default ActiveInput
